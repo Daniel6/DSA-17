01b9c2e39af6a9388d48fcb70866a046a074dbf9
dbishop <dbishop.rwc@gmail.com>
2017-01-20 16:20:12 -0500

    day 1 hw in progress

--- day01/src/MyArrayList.java
+++ day01/src/MyArrayList.java
@@ -1,35 +1,77 @@
+import java.util.Arrays;
+
 public class MyArrayList {
     private Cow[] elems;
 	private int size;
+	private int nextEmptyIndex = 0;
 
 	public MyArrayList() {
-		// TODO
+		size = 10;
+		elems = new Cow[10];
 	}
 
 	public MyArrayList(int capacity) {
-		// TODO
+		size = capacity;
+		elems = new Cow[capacity];
 	}
 
 	public void add(Cow c) {
-		// TODO
+		elems[nextEmptyIndex] = c;
+		nextEmptyIndex++;
+		resize();
 	}
 
 	public int size() {
-		// TODO
-		return -1;
+		// The next empty index is always equivalent to the number of cows in the array
+		return nextEmptyIndex;
 	}
 
 	public Cow get(int index) {
-		// TODO
-		return null;
+		if (index >= nextEmptyIndex) {
+			throw new IndexOutOfBoundsException("No cow at that index");
+		} else {
+			return elems[index];
+		}
 	}
 
 	public Cow remove(int index) {
-		// TODO
-		return null;
+		Cow removedCow = elems[index];
+		for (int i = index; i < nextEmptyIndex; i++) {
+            elems[i] = elems[i + 1];
+        }
+        nextEmptyIndex--; // the number of cows in the array has decreased by 1
+
+        resize();
+		return removedCow;
 	}
 
 	public void add(int index, Cow c) {
-		// TODO
+	    // You can only insert cows in between indices containing cows.
+        if (index >= nextEmptyIndex) {
+            throw new IndexOutOfBoundsException("Cannot insert cow between non-cow indices");
+        } else {
+            // shift all the elements to the right of the insertion index
+            for (int i = nextEmptyIndex; i > index; i--) {
+                elems[i] = elems[i-1];
+            }
+            elems[index] = c;
+            nextEmptyIndex++;
+        }
+
+        resize();
 	}
+
+	/*
+	    If the arraylist is <25% full, halve the capacity.
+	    If the arraylist is full, double the capacity.
+	 */
+	private void resize() {
+        if (nextEmptyIndex >= size) {
+            elems = Arrays.copyOf(elems, size * 2);
+            size *= 2;
+        } else if (nextEmptyIndex < size/4 && nextEmptyIndex > 0) {
+            elems = Arrays.copyOf(elems, size / 2);
+            size /= 2;
+        }
+    }
 }
