c2ad223175641a5123a0b82bf2ba40c27c08c15f
dbishop <dbishop.rwc@gmail.com>
2017-02-14 16:16:18 -0500

    folders created

---
+++ search-engine/day01/src/TermCounter.java
@@ -0,0 +1,90 @@
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class TermCounter {
+
+	private Map<String, Integer> map;
+	private String label;
+	private int size;
+
+	public TermCounter(String label) {
+		this.label = label;
+		this.size = 0;
+		this.map = new HashMap<String, Integer>();
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public void processElements(Elements paragraphs) {
+		for (Node node: paragraphs) {
+			processTree(node);
+		}
+	}
+
+	public void processTree(Node root) {
+		// NOTE: we could use select to find the TextNodes, but since
+		// we already have a tree iterator, let's use it.
+		for (Node node: new WikiNodeIterable(root)) {
+			if (node instanceof TextNode) {
+				processText(((TextNode) node).text());
+			}
+		}
+	}
+
+	public void processText(String text) {
+		// replace punctuation with spaces, convert to lower case, and split on whitespace
+		String[] array = text.replaceAll("\\pP", " ").toLowerCase().split("\\s+");
+
+		for (int i = 0; i < array.length; i++) {
+			incrementTermCount(array[i]);
+			}
+	}
+
+	public void incrementTermCount(String term) {
+		put(term, map.getOrDefault(term, 0) + 1);
+	}
+
+	public void put(String term, int count) {
+		size += count - map.getOrDefault(term, 0);
+		map.put(term, count);
+	}
+
+	public Integer get(String term) {
+		return map.getOrDefault(term, 0);
+	}
+
+	public Set<String> keySet() {
+		return map.keySet();
+	}
+
+	public void printCounts() {
+		for (String key: keySet()) {
+			Integer count = get(key);
+			System.out.println(key + ", " + count);
+		}
+		System.out.println("Total of all counts = " + size());
+	}
+
+	public static void main(String[] args) throws IOException {
+		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+
+		WikiFetcher wf = new WikiFetcher();
+		Elements paragraphs = wf.fetchWikipedia(url);
+
+		TermCounter counter = new TermCounter(url.toString());
+		counter.processElements(paragraphs);
+		counter.printCounts();
+	}
+}
