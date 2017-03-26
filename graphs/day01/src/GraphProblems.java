af410d388d6463de0b9e8e0f28006e9d5f081a58
dbishop <dbishop.rwc@gmail.com>
2017-03-26 17:29:41 -0400

    graphs day 1 done

--- graphs/day01/src/GraphProblems.java
+++ graphs/day01/src/GraphProblems.java
@@ -1,19 +1,91 @@
-import java.util.List;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Queues;
+import com.google.common.collect.Sets;
+
+import java.util.*;
 
 public class GraphProblems {
 
     public static boolean connected(Graph g, int v, int u) {
-        // TODO
+        Queue<Integer> nodesToLookAt = Queues.newArrayDeque();
+        nodesToLookAt.add(v);
+        Set<Integer> seen = Sets.newHashSet();
+        seen.add(v);
+
+        while (!nodesToLookAt.isEmpty()) {
+            int thisNode = nodesToLookAt.poll();
+            for (int i : g.getNeighbors(thisNode)) {
+                if (i == u) {
+                    return true;
+                }
+                if (!seen.contains(i)) {
+                    nodesToLookAt.add(i);
+                    seen.add(i);
+                }
+            }
+        }
+
         return false;
     }
 
     public static List<Integer> topologicalOrder(Digraph g) {
-        // TODO
-        return null;
+        HashMap<Integer, Integer> indegrees = Maps.newHashMap();
+        for (int node : g.vertices()) {
+            indegrees.putIfAbsent(node, 0);
+            for (int neighbor : g.getNeighbors(node)) {
+                indegrees.put(neighbor, indegrees.getOrDefault(neighbor, 0) + 1);
+            }
+        }
+
+        Queue<Integer> nodesToVisit = Queues.newArrayDeque();
+        for (int node : g.vertices()) {
+            if (indegrees.get(node) == 0) {
+                nodesToVisit.add(node);
+            }
+        }
+
+        List<Integer> topOrder = Lists.newArrayList();
+        while (!nodesToVisit.isEmpty()) {
+            int node = nodesToVisit.poll();
+            topOrder.add(node);
+            for (int neighbor : g.getNeighbors(node)) {
+                indegrees.put(neighbor, indegrees.get(neighbor) - 1);
+                if (indegrees.get(neighbor) == 0) {
+                    nodesToVisit.add(neighbor);
+                }
+            }
+        }
+
+        return topOrder;
     }
 
     public static boolean hasCycle(UndirectedGraph g) {
-        // TODO
+        HashSet<Integer> seen = Sets.newHashSet();
+
+        for (int node : g.vertices()) {
+            if (!seen.contains(node)) {
+                if (hasCycleHelper(g, node, seen, -1)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean hasCycleHelper(UndirectedGraph g, int v, HashSet<Integer> seen, int parent) {
+        seen.add(v);
+        for (int neighbor : g.getNeighbors(v)) {
+            if (!seen.contains(neighbor)) {
+                if (hasCycleHelper(g, neighbor, seen, v)) {
+                    return true;
+                }
+            } else if (neighbor != parent) {
+                return true;
+            }
+        }
+
         return false;
     }
 
--- graphs/day01/src/ReviewProblem.java
+++ graphs/day01/src/ReviewProblem.java
@@ -1,8 +1,33 @@
 public class ReviewProblem {
 
     public static int minimumLengthSubArray(int[] A, int desiredSum) {
-        // TODO
-        return 0;
+        int minInd = 0;
+        int maxInd = 0;
+        int runningSum = 0;
+        int minLen = 0;
+
+        while (maxInd < A.length) {
+            while (maxInd < A.length && runningSum < desiredSum) {
+                runningSum += A[maxInd];
+                maxInd++;
+            }
+
+            while (minInd < maxInd && runningSum - A[minInd] >= desiredSum) {
+                runningSum -= A[minInd];
+                minInd++;
+            }
+
+            if (maxInd - minInd < minLen || minLen == 0) {
+                minLen = maxInd - minInd;
+            }
+
+            if (minInd < maxInd) {
+                runningSum -= A[minInd];
+                minInd++;
+            }
+        }
+
+        return minLen;
     }
 
 }
