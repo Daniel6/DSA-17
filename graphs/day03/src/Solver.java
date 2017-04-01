98d08bab8befa8725f24bc5b5ccc6d7034543c59
dbishop <dbishop.rwc@gmail.com>
2017-04-01 10:26:35 -0400

    shifting cleanup

--- graphs/day03/src/Solver.java
+++ graphs/day03/src/Solver.java
@@ -68,12 +68,20 @@ public class Solver {
     public Solver(Board initial) {
     	State root = new State(initial, 0, null);
 
-        PriorityQueue<State> pq = PriorityQueue.
+        PriorityQueue<State> pq = new PriorityQueue<>(new Comparator<State>() {
+            @Override
+            public int compare(State o1, State o2) {
+                return o1.compareTo(o2);
+            }
+        });
 
-
-        for (Board neighbor : root.board.neighbors()) {
-    	    State n = new State(neighbor, root.moves + 1, root);
-    	    pq.
+        pq.add(root);
+        while (!pq.isEmpty()) {
+            State s = pq.poll();
+            for (Board neighbor : s.board.neighbors()) {
+                State n = new State(neighbor, s.moves + 1, s);
+                pq.add(n);
+            }
         }
     }
 
