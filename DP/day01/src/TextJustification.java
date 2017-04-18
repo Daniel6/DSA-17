b3f0214f15285890c97c4ff18d194984c90a2b55
dbishop <dbishop.rwc@gmail.com>
2017-04-17 20:54:17 -0400

    w

--- DP/day01/src/TextJustification.java
+++ DP/day01/src/TextJustification.java
@@ -5,41 +5,49 @@ import java.util.List;
 public class TextJustification {
 
     private static int[] memos;
+    private static int[] breaks;
 
     public static List<Integer> justifyText(String[] w, int m) {
         memos = new int[w.length];
         for (int i = 0; i < w.length; i++) {
             memos[i] = Integer.MAX_VALUE;
         }
-        helper(w, 0, m, 0, new ArrayList<>());
+
+        breaks = new int[w.length];
+
+        helper(w, 0, m, 0);
 
         System.out.println(Arrays.toString(memos));
+        System.out.println(Arrays.toString(breaks));
         List<Integer> soln = new ArrayList<Integer>();
+
+        int i = 0;
+        while (i >= 0 && i < w.length) {
+            soln.add(i);
+            i = breaks[i];
+        }
 
         return soln;
     }
 
-    private static int helper(String[] words, int wordsProcessed, int lineLength, int oldCost, List<Integer> breaks) {
+    private static int helper(String[] words, int wordsProcessed, int lineLength, int oldCost) {
         if (wordsProcessed == words.length) {
-            memos[wordsProcessed - 1] = oldCost;
+            memos[wordsProcessed-1] = oldCost;
+            breaks[wordsProcessed-1] = wordsProcessed;
             return oldCost;
         }
         if (memos[wordsProcessed] < oldCost) {
             return memos[wordsProcessed];
         }
 
-
         for (int i = wordsProcessed; i < words.length; i++) {
             int cost = cost(words, wordsProcessed, i, lineLength);
             if (cost >= 0) {
-                List<Integer> newBreaks = new ArrayList<>();
-                for (int b : breaks) {
-                    newBreaks.add(b);
+                int soln = helper(words, i+1, lineLength, cost + oldCost);
+                if (soln < memos[wordsProcessed]) {
+                    memos[wordsProcessed] = soln;
+                    breaks[wordsProcessed] = i+1;
                 }
-                newBreaks.add(i+1);
-
-                int soln = helper(words, i+1, lineLength, cost + oldCost, newBreaks);
-                if (soln < memos[wordsProcessed]) memos[wordsProcessed] = soln;
             } else {
                 break;
             }
@@ -55,14 +63,5 @@ public class TextJustification {
         }
         sum += j-i;
         return (int) Math.pow(lineLength - sum, 3);
-    }
-
-    private static class Solution {
-        public static int cost;
-        public static List<Integer> breaks;
-        public Solution(int cost, List<Integer> breaks) {
-            this.cost = cost;
-            this.breaks = breaks;
-        }
     }
 }
