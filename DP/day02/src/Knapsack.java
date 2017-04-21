3d90c3372d8c24f9eeeb52385b622b39fe943a2c
dbishop <dbishop.rwc@gmail.com>
2017-04-20 23:34:38 -0400

    WIP DP day 2

--- DP/day02/src/Knapsack.java
+++ DP/day02/src/Knapsack.java
@@ -1,8 +1,49 @@
+import java.util.Arrays;
+
 public class Knapsack {
 
     public static int maxValue(int knapsackSize, int[] S, int[] V) {
-        // TODO
-        return -1;
+        // Memo maps from # of items considered -> max possible capacity
+        int[][] memos = new int[S.length][knapsackSize + 1];
+        for (int i = 0; i < memos.length; i++) {
+            for (int j = 0; j < knapsackSize+1; j++) {
+                memos[i][j] = -1;
+            }
+        }
+
+        System.out.println("S " + Arrays.toString(S));
+        System.out.println("V " + Arrays.toString(V));
+        int soln = helper(0, 0, 0, S, V, knapsackSize, memos);
+
+        System.out.println(soln);
+        return soln;
+    }
+
+    private static int helper(int i, int totalValue, int totalSpace, int[] S, int[] V, int knapsackSize, int[][] memos) {
+        if (i >= S.length) {
+            return totalValue;
+        }
+
+        if (totalSpace == knapsackSize) {
+            memos[i][totalSpace] = totalValue;
+            return totalValue;
+        } else if (totalSpace > knapsackSize) {
+            return -1;
+        }
+
+        if (memos[i][totalSpace] == -1) {
+            if (totalSpace + S[i] <= knapsackSize) {
+                memos[i][totalSpace] = Math.max(
+                    memos[i][totalSpace],
+                    helper(i+1, totalValue + V[i], totalSpace + S[i], S, V, knapsackSize, memos));
+            }
+            memos[i][totalSpace] = Math.max(
+                memos[i][totalSpace],
+                helper(i+1, totalValue, totalSpace, S, V, knapsackSize, memos)
+            );
+        }
+
+        return memos[i][totalSpace];
     }
 
 }
--- DP/day02/src/Regex.java
+++ DP/day02/src/Regex.java
@@ -1,8 +1,91 @@
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import java.util.HashMap;
+import java.util.List;
+
 public class Regex {
 
     public static boolean isMatch(String s, String p) {
-        // TODO
-        return false;
+        MyFSM fsm = new MyFSM(p);
+        return fsm.matchString(s);
     }
 
+    private static class MyFSM {
+        private FSMNode start;
+
+        public MyFSM(String regex) {
+            start = new FSMNode();
+
+            int i = 0;
+            FSMNode currNode = start;
+            FSMNode prevNode = null;
+
+            char c;
+            char prev = '\0';
+            while (i < regex.length()) {
+                c = regex.charAt(i);
+                if (c == '*') {
+                    // 0 or more of previous char
+                    currNode = prevNode;
+                    currNode.setCircularRedirect(String.valueOf(prev));
+                } else {
+                    // Char is a letter in the alphabet or a period
+                    FSMNode newNode = new FSMNode();
+                    currNode.setRedirect(String.valueOf(c), newNode);
+                    prevNode = currNode;
+                    currNode = newNode;
+                }
+                prev = c;
+                i++;
+            }
+
+            currNode.setAsSuccessState();
+        }
+
+        public boolean matchString(String sample) {
+            FSMNode curr = start;
+            for (int i = 0; i < sample.length(); i++) {
+                String s = String.valueOf(sample.charAt(i));
+
+                curr = curr.get(s);
+                if (curr == null) return false;
+            }
+
+            return curr.isSuccess();
+        }
+    }
+
+    private static class FSMNode {
+        private HashMap<String, FSMNode> redirects;
+        private boolean isSuccess;
+
+        public FSMNode() {
+            redirects = Maps.newHashMap();
+            isSuccess = false;
+        }
+
+        public FSMNode get(String key) {
+            System.out.println(key + " in " + redirects);
+            if (redirects.containsKey(key)) return redirects.get(key);
+            if (redirects.containsKey(".")) return redirects.get(".");
+            return null;
+        }
+
+        public void setCircularRedirect(String key) {
+            redirects.put(key, this);
+        }
+
+        public void setRedirect(String key, FSMNode node) {
+            redirects.put(key, node);
+        }
+
+        public void setAsSuccessState() {
+            isSuccess = true;
+        }
+
+        public boolean isSuccess() {
+            return isSuccess;
+        }
+    }
 }
